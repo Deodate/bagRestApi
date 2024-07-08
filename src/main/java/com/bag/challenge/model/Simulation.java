66bd5a28df7674c0603ba10daefbb58bbfc6b9fd
Deodate <ajadeo04@gmail.com>
2024-07-08 11:17:31 +0200

    Progress

---
+++ src/main/java/com/bag/challenge/model/Simulation.java
@@ -0,0 +1,16 @@
+package com.bag.challenge.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Simulation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private Long userId;
+}
